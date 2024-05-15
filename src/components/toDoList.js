71b1592b2254a7726450f300f114ac2b17afaa33
Sidnotsadist <102277137+sid1273@users.noreply.github.com>
2024-05-15 14:10:23 +0530

    Added Local Storage

--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 
-
-
-
 const ToDoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState(() => {
+    const todosJson = localStorage.getItem('todos');
+    return todosJson ? JSON.parse(todosJson) : [];
+  });
 
+  const [inputValue, setInputValue] = useState('');
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -31,11 +34,11 @@ const ToDoList = () => {
   };
 
   const handleSaveEdit = (index, newText) => {
-    if (newText.trim() !== ''){
-    const newTodos = [...todos];
-    newTodos[index].text = newText;
-    newTodos[index].isEditing = false;
-    setTodos(newTodos);
+    if (newText.trim() !== '') {
+      const newTodos = [...todos];
+      newTodos[index].text = newText;
+      newTodos[index].isEditing = false;
+      setTodos(newTodos);
     }
   };
 
@@ -45,8 +48,8 @@ const ToDoList = () => {
     setTodos(newTodos);
   };
 
-  const handleDeleteDoneTodos = () => {
-    const newTodos = todos.filter(todo => !todo.isDone);
+  const handleDeleteToDo = (index) => {
+    const newTodos = todos.filter((todo, i) => i !== index);
     setTodos(newTodos);
   };
 
@@ -58,54 +61,66 @@ const ToDoList = () => {
 
   return (
     <>
-    <Container className='d-flex justify-content-center' style={{margin:'20px 20px'}}>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Add Task here"      
-      >
-      <Form.Control type="text" 
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="name@example.com" />
-      </FloatingLabel>
-      <Button variant="outline-secondary"  style={{margin: '5px'}}action onClick={handleAddToDo}>Add</Button>
+      <Container className='d-flex justify-content-center' style={{ margin: '20px 20px' }}>
+        <FloatingLabel controlId="floatingInput" label="Add Task here">
+          <Form.Control
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+        </FloatingLabel>
+        <Button variant="outline-secondary" style={{ margin: '5px' }} onClick={handleAddToDo}>
+          Add
+        </Button>
       </Container>
 
-
-      <ListGroup style={{backgroundColor: 'blue'}} className='d-flex justify-content-center'>
+      <ListGroup className='d-flex justify-content-center'>
         {todos.map((todo, index) => (
-          <ListGroup.Item style={{textWrap: 'balance'}} key={index}>
+          <ListGroup.Item key={index}>
             {todo.isEditing ? (
-                <FloatingLabel
-                controlId="floatingInput"
-                label="Edit Task Here"
-                className="mb-3"
-              >
-                <Form.Control type="text" 
-                className='d-flex justify-content-center' 
-                value={todo.text} 
-                onChange={(e) => handleEditInputChange(e, index)}
-                onBlur={() => handleSaveEdit(index, todo.text)} placeholder='Edit task here'autoFocus />
+              <FloatingLabel controlId="floatingInput" label="Edit Task Here" className="mb-3">
+                <Form.Control
+                  type="text"
+                  className='d-flex justify-content-center'
+                  value={todo.text}
+                  onChange={(e) => handleEditInputChange(e, index)}
+                  onBlur={() => handleSaveEdit(index, todo.text)}
+                  placeholder='Edit task here'
+                  autoFocus
+                />
               </FloatingLabel>
-
             ) : (
               <span style={{ textDecoration: todo.isDone ? 'line-through' : 'none' }}>
                 {todo.text}
               </span>
             )}
-            <Button  size='sm' style={{margin: '5px'}} variant="outline-secondary" onClick={() => handleDone(index)}>{todo.isDone ? 'Undo' : 'Done'}</Button>
+            <Button
+              size='sm'
+              style={{ margin: '5px' }}
+              variant="outline-secondary"
+              onClick={() => handleDone(index)}
+            >
+              {todo.isDone ? 'Undo' : 'Done'}
+            </Button>
+
+            {!todo.isDone && !todo.isEditing && (
+              <Button size='sm' variant="outline-secondary" onClick={() => handleEditToDo(index)}>
+                Edit
+              </Button>
+            )}
             
-            {!todo.isDone && !todo.isEditing && 
-            (<Button size='sm' variant="outline-secondary" onClick={() => handleEditToDo(index)}>Edit</Button>)}
+            {todo.isDone && (
+              <Button
+                size='sm'
+                variant="outline-secondary"
+                onClick={() => handleDeleteToDo(index)}
+              >
+                Delete
+              </Button>
+            )}
           </ListGroup.Item>
         ))}
       </ListGroup>
-
-      <Container className='d-flex justify-content-center'>
-      {todos.some(todo => todo.isDone) && (
-        <Button style={{margin: '10px 10px'}}  variant="outline-secondary" onClick={handleDeleteDoneTodos}>Delete All Done Tasks</Button>
-      )}
-      </Container>
     </>
   );
 };
